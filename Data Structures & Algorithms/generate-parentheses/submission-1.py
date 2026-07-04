97df000d6021cb15457597b8d5555453a7a30b26
Ahilesh Roy B J <146226842+AHILESHROY@users.noreply.github.com>
2026-07-04 22:04:39 +0530

    Add: generate-parentheses - submission-1

---
+++ Data Structures & Algorithms/generate-parentheses/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res=[]
+        string=[]
+        
+        def bt(Open,Close):
+            
+            if Open==Close==n:
+                res.append("".join(string))
+                return
+            if Open<n:
+                string.append("(")
+
+                bt(Open+1,Close)
+                string.pop()
+            if Close<Open:
+                string.append(")")
+                bt(Open,Close+1)
+                string.pop()
+
+        bt(0,0)
+        return res
